a4296618b31afdb0876feea47442251d43ac0654
Halil Focic <halil.focic@zendev.se>
2024-05-28 09:53:01 +0200

    mistakes were made

--- Formula/assetm.rb
+++ Formula/assetm.rb
@@ -1,7 +1,7 @@
 class Assetm < Formula
   desc "Asset manager written in rust that let's you import assets into your project using terminal"
   homepage "https://github.com/HalilFocic/rust-asset-manager"
-  url "https://github.com/HalilFocic/rust-asset-manager/archive/refs/tags/v1.0.1.tar.gz"
+  url "https://github.com/HalilFocic/rust-asset-manager/releases/download/v1.0.2/assetm.tar.gz"
   sha256 "d9c77093e7678c2bf801b1c801785d720a061db7285deca254db1e3406a65183"
   license "MIT"
 
